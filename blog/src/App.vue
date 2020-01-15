0bfa129425086fc941e6adf2136ac3949f0f6a99
zhumingxiao <zhumingxiao@megvii.com>
2020-01-15 14:44:32 +0000

    Feature: init website

---
+++ blog/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <body>
+      <Menu class="nav" mode="horizontal" :theme="navTheme" @on-select="changeRoute">
+        <div class="navlayout">
+          <Menu-item><Icon type="md-images"/>手账</Menu-item>
+          <Menu-item><Icon type="ios-paper-plane"/>树莓派</Menu-item>
+          <Menu-item><Icon type="ios-planet"/>读书笔记</Menu-item>
+        </div>
+      </Menu>
+      <router-view/>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      navTheme: 'light'
+    }
+  },
+
+  methods: {
+    changeRoute () {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.nav {
+  position: fixed !important;
+  left: 0;
+  top: 0;
+  width: 100vw;
+}
+
+.navlayout {
+  margin-left: 50px;
+}
+
+</style>
+
---
+++ blog/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <pre>Welcome to Time-Limit's website</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
